63e14489806aab0499e3b4da63eaf29464ca4e29
wyh <1146900419@qq.com>
2023-10-28 09:23:31 +0800

    master

--- src/app/example.java
+++ src/app/example.java
@@ -6,15 +6,7 @@ public class example {
     public static void main(String[] args) throws IOException {
         CSapp sender = new CSapp(20,1,30,30,"sender","C:\\Users\\wyh\\Desktop\\lab2\\src\\app\\source\\a.txt");
         CSapp recieve = new CSapp(80,1,30,30,"reciever","C:\\Users\\wyh\\Desktop\\lab2\\src\\app\\source\\b.txt");
-        new Thread(() -> {
-            try {
-                recieve.receive();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
-        new Thread(() -> {
-            sender.send();
+        ne
         }).start();
     }
 
